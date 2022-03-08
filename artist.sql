c0c1c96ae520189df7984d16923e33eff6e7fdad
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-07 17:14:43 -0700

    Invoices section complete, artist file fixed, employee file fixed.

--- artist.sql
+++ artist.sql
@@ -1,11 +1,11 @@
-insert into artists (artist_name)
+insert into artist (artist_name)
     values ('Black Sabbath'), ('Breaking Benjamin'), ('Coheed and Cambria'),
             ('Linkin Park'), ('Glitch Mob');
 
-select * from artists order by artist_name desc limit 10;
+select * from artist order by name desc limit 10;
 
-select * from artists order by artist_name limit 5;
+select * from artist order by name limit 5;
 
-select * from artists where name like 'Black%';
+select * from artist where name like 'Black%';
 
-select * from artists where name like '%Black%';
+select * from artist where name like '%Black%';
